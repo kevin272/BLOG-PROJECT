faf507b920cb0b421622fa451f0972c27fe39f2d
Kebin <keninadod@gmail.com>
2025-01-04 19:20:44 +0545

    update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  base: '/BLOG-PROJECT/', // Set the base path for GitHub Pages deployment
+  site: 'https://kevin272.github.io/BLOG-PROJECT', // Optional: set the site URL
+});
